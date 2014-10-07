c79a07bb74c58ee2d8a8f9366850ff89f8eca4d2
xeronimus <xeronimus@gmail.com>
2014-10-07 09:29:09 +0200

    Update 2014-10-07T07:29:09.163Z

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-angular.module('cheatSheets',['ui.bootstrap']);
+angular.module('cheatSheets', ['ui.bootstrap']);
 
 function CheatSheetController($location, CheatSheetStore) {
-    var vm = this;
-    var LOADING = {id: 1, message: 'Loading...'};
-    var OK = {id: 2, message: 'Ready'};
-    var ERROR = {id: 3, message: 'Something went wrong!'};
-    var NO_CS_SELECTED = {id: 4 };
+  var vm = this;
+  var LOADING = {id: 1, message: 'Loading...'};
+  var OK = {id: 2, message: 'Ready'};
+  var ERROR = {id: 3, message: 'Something went wrong!'};
+  var NO_CS_SELECTED = {id: 4 };
 
+  vm.menuCollapsed = true;
+  vm.currentCheatSheet = undefined;
+  vm.status = LOADING;
+  vm.selectCheetSheet = selectCheetSheet;
+
+  function selectCheetSheet(givenCheatSheetId) {
     vm.menuCollapsed = true;
-    vm.currentCheatSheet = undefined;
+
+    if (!givenCheatSheetId || givenCheatSheetId.length < 1) {
+      vm.status = NO_CS_SELECTED;
+      return;
+    }
+
     vm.status = LOADING;
-    vm.selectCheetSheet = selectCheetSheet;
+    CheatSheetStore.getCheatSheetContent(givenCheatSheetId + '.json').then(function (data) {
+      vm.currentCheatSheet = data;
+      vm.status = OK;
+    }, function () {
+      vm.status = ERROR;
+      vm.status.message = 'Could not load cheat sheet "' + givenCheatSheetId + '"';
+    });
+  }
 
-    function selectCheetSheet(givenCheatSheetId) {
-        vm.menuCollapsed = true;
+  function loadIndex() {
+    CheatSheetStore.getCheatSheetIndex().then(function (data) {
+      vm.index = data;
+      selectCheetSheet($location.path().substr(1));
+    }, function () {
+      vm.status = ERROR;
+      vm.status.message = 'Could not load cheat sheet index!';
+    });
+  }
 
-        if (!givenCheatSheetId || givenCheatSheetId.length < 1) {
-            vm.status = NO_CS_SELECTED;
-            return;
-        }
-
-        vm.status = LOADING;
-        CheatSheetStore.getCheatSheetContent(givenCheatSheetId + '.json').then(function (data) {
-            vm.currentCheatSheet = data;
-            vm.status = OK;
-        }, function () {
-            vm.status = ERROR;
-            vm.status.message = 'Could not load cheat sheet "' + givenCheatSheetId + '"';
-        });
-    }
-
-    function loadIndex() {
-        CheatSheetStore.getCheatSheetIndex().then(function (data) {
-            vm.index = data;
-            selectCheetSheet($location.path().substr(1));
-        }, function () {
-            vm.status = ERROR;
-            vm.status.message = 'Could not load cheat sheet index!';
-        });
-    }
-
-    loadIndex();
+  loadIndex();
 }
 
 angular.module('cheatSheets').controller('CheatSheetController', CheatSheetController);
@@ -51,34 +51,54 @@ angular.module('cheatSheets').controller('CheatSheetController', CheatSheetContr
  */
 function CheatSheetStore($http, $q) {
 
-    var csIndex;
+  var csIndex;
+  var cheatSheetCache = {};
 
-    return {
-        getCheatSheetContent: getCheatSheetContent,
-        getCheatSheetIndex: getCheatSheetIndex
-    };
+  return {
+    getCheatSheetContent: getCheatSheetContent,
+    getCheatSheetIndex: getCheatSheetIndex,
+    clearCache: clearCache
+  };
 
-    function getCheatSheetContent(csFileName) {
-        var deferred = $q.defer();
-        $http.get('storage/' + csFileName).success(function (data) {
-            var csData = angular.fromJson(data);
-            deferred.resolve(csData);
-        }).error(function () {
-            deferred.reject();
-        });
-        return deferred.promise;
+
+  function getCheatSheetContent(csFileName) {
+    var deferred = $q.defer();
+    if (cheatSheetCache[csFileName]) {
+      deferred.resolve(cheatSheetCache[csFileName]);
+    } else {
+      loadCheatSheetContent(csFileName).then(function (data) {
+        cheatSheetCache[csFileName] = data;
+        deferred.resolve(data);
+      }, deferred.reject);
     }
+    return deferred.promise;
+  }
 
-    function getCheatSheetIndex() {
-        var deferred = $q.defer();
-        $http.get('storage/index.json').success(function (data) {
-            csIndex = angular.fromJson(data);
-            deferred.resolve(csIndex);
-        }).error(function () {
-            deferred.reject();
-        });
-        return deferred.promise;
-    }
+  function loadCheatSheetContent(csFileName) {
+    var deferred = $q.defer();
+    $http.get('storage/' + csFileName).success(function (data) {
+      var csData = angular.fromJson(data);
+      deferred.resolve(csData);
+    }).error(function () {
+      deferred.reject();
+    });
+    return deferred.promise;
+  }
+
+  function getCheatSheetIndex() {
+    var deferred = $q.defer();
+    $http.get('storage/index.json').success(function (data) {
+      csIndex = angular.fromJson(data);
+      deferred.resolve(csIndex);
+    }).error(function () {
+      deferred.reject();
+    });
+    return deferred.promise;
+  }
+
+  function clearCache() {
+
+  }
 
 }
 
@@ -89,21 +109,21 @@ angular.module('cheatSheets').factory('CheatSheetStore', CheatSheetStore);
  */
 function EveryOtherItemFilter() {
 
-    return function (items, even) {
-        if (angular.isUndefined(items) || !angular.isArray(items)) {
-            return undefined;
-        }
+  return function (items, even) {
+    if (angular.isUndefined(items) || !angular.isArray(items)) {
+      return undefined;
+    }
 
-        var filteredItems = [];
-        var match = even ? 1 : 0;
-        items.forEach(function (element, index) {
-            if (index % 2 === match) {
-                filteredItems.push(element);
-            }
-        });
+    var filteredItems = [];
+    var match = even ? 1 : 0;
+    items.forEach(function (element, index) {
+      if (index % 2 === match) {
+        filteredItems.push(element);
+      }
+    });
 
-        return filteredItems;
-    };
+    return filteredItems;
+  };
 
 }
 
