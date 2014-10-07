5480e0561c1f5f513a210b54c08a53c306c257e0
xeronimus <xeronimus@gmail.com>
2014-10-07 09:40:55 +0200

    cache cheat sheets

--- app/scripts/CheatSheetStore.js
+++ app/scripts/CheatSheetStore.js
@@ -5,13 +5,29 @@
 function CheatSheetStore($http, $q) {
 
   var csIndex;
+  var cheatSheetCache = {};
 
   return {
     getCheatSheetContent: getCheatSheetContent,
-    getCheatSheetIndex: getCheatSheetIndex
+    getCheatSheetIndex: getCheatSheetIndex,
+    clearCache: clearCache
   };
 
+
   function getCheatSheetContent(csFileName) {
+    var deferred = $q.defer();
+    if (cheatSheetCache[csFileName]) {
+      deferred.resolve(cheatSheetCache[csFileName]);
+    } else {
+      loadCheatSheetContent(csFileName).then(function (data) {
+        cheatSheetCache[csFileName] = data;
+        deferred.resolve(data);
+      }, deferred.reject);
+    }
+    return deferred.promise;
+  }
+
+  function loadCheatSheetContent(csFileName) {
     var deferred = $q.defer();
     $http.get('storage/' + csFileName).success(function (data) {
       var csData = angular.fromJson(data);
@@ -31,6 +47,10 @@ function CheatSheetStore($http, $q) {
       deferred.reject();
     });
     return deferred.promise;
+  }
+
+  function clearCache() {
+
   }
 
 }
--- test/CheatSheetStoreTest.js
+++ test/CheatSheetStoreTest.js
@@ -21,26 +21,31 @@ describe('CheatSheetStore', function () {
     expect(CheatSheetStore).not.to.be(undefined);
   });
 
-  it('must load return cheat sheet index', function (done) {
+  describe('#getCheatSheetIndex()', function () {
 
-    var dummyIndexContent = '["one","two"]';
 
-    $httpBackend.expectGET('storage/index.json').respond(200, dummyIndexContent);
+    it('must load return cheat sheet index', function (done) {
 
-    var promise = CheatSheetStore.getCheatSheetIndex();
+      var dummyIndexContent = '["one","two"]';
 
-    promise.then(function (data) {
-      expect(data).to.be.a('array');
-      expect(data[0]).to.be('one');
-      expect(data[1]).to.be('two');
-      done();
+      $httpBackend.expectGET('storage/index.json').respond(200, dummyIndexContent);
+
+      var promise = CheatSheetStore.getCheatSheetIndex();
+
+      promise.then(function (data) {
+        expect(data).to.be.a('array');
+        expect(data[0]).to.be('one');
+        expect(data[1]).to.be('two');
+        done();
+      });
+      $httpBackend.flush();
     });
-    $httpBackend.flush();
+
   });
 
-  it('must load and return cheat sheet by fileName', function (done) {
+  describe('#getCheatSheetContent()', function () {
 
-    var dummyCheatSheet = {
+    var dummyCheatSheetOne = {
       'id': 'superCheatCheet',
       'title': 'The Best',
       'chapters': [
@@ -60,17 +65,60 @@ describe('CheatSheetStore', function () {
       ]
     };
 
-    $httpBackend.expectGET('storage/one.json').respond(200, angular.toJson(dummyCheatSheet));
 
-    var promise = CheatSheetStore.getCheatSheetContent('one.json');
-
-    promise.then(function (data) {
-      expect(data).to.be.a('object');
-      expect(data).to.eql(dummyCheatSheet);
-      done();
+    beforeEach(function () {
+      CheatSheetStore.clearCache();
     });
-    $httpBackend.flush();
+
+    it('must load and return cheat sheet by fileName', function (done) {
+
+      $httpBackend.expectGET('storage/one.json').respond(200, angular.toJson(dummyCheatSheetOne));
+      var promise = CheatSheetStore.getCheatSheetContent('one.json');
+
+      promise.then(function (data) {
+        expect(data).to.be.a('object');
+        expect(data).to.eql(dummyCheatSheetOne);
+        done();
+      });
+      $httpBackend.flush();
+    });
+
+    it('must return cheat sheet from cache if it was already loaded', function (done) {
+
+      // first load from backend via HTTP GET
+      $httpBackend.expectGET('storage/one.json').respond(200, angular.toJson(dummyCheatSheetOne));
+      var promise = CheatSheetStore.getCheatSheetContent('one.json');
+
+      promise.then(function (data) {
+        expect(data).to.eql(dummyCheatSheetOne);
+        // second time, the content is served from cache
+        CheatSheetStore.getCheatSheetContent('one.json').then(function () {
+          done();
+        });
+      });
+      $httpBackend.flush();
+
+    });
+
+    it('must return different Cheat sheet from backedn', function (done) {
+
+      // first load from backend via HTTP GET
+      $httpBackend.expectGET('storage/one.json').respond(200, angular.toJson(dummyCheatSheetOne));
+      var promise = CheatSheetStore.getCheatSheetContent('one.json');
+
+      promise.then(function (data) {
+        expect(data).to.eql(dummyCheatSheetOne);
+        // again, loading from backend
+        $httpBackend.expectGET('storage/two.json').respond(200, angular.toJson(dummyCheatSheetOne));
+        var promiseTwo = CheatSheetStore.getCheatSheetContent('two.json');
+        promiseTwo.then(function () {
+          done();
+        });
+      });
+      $httpBackend.flush();
+
+    });
+
+
   });
-
-
 });
